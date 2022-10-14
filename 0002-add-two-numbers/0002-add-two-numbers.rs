3697b8d5aa86f510c6339ad112bf62d48e39cfbf
lucasfrailev <47170229+lucasfrailev@users.noreply.github.com>
2022-10-14 15:33:18 -0700

    Time: 7 ms (58.27%), Space: 2.1 MB (65.35%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.rs
+++ 0002-add-two-numbers/0002-add-two-numbers.rs
@@ -16,73 +16,27 @@
 // }
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        impl ListNode {
-            #[inline]
-            fn add(&mut self, value:i32){
-                let mut selfreference = self;
-                loop {
-                    match selfreference.next {
-                        Some(ref mut nextnode) => { 
-                            selfreference = nextnode;
-                            },
-                        None => {
-                            selfreference.next = Some(Box::new(ListNode::new(value)));
-                            break;
-                            },
-                    }
-                }
-            }
-
-            fn pop(&mut self) -> Option<i32> {
-                match self.next {
-                        Some(ref nextnode) => { 
-                            let value = nextnode.val;
-                            *self = *nextnode.clone();
-                            Some(value)
-                            },
-                        None => None,
-                }   
-            }
-        }
         match (l1, l2) {
             (None, None) => None,
             (Some(n), None) | (None, Some(n)) => Some(n), 
-            (Some(ref mut list1), Some(ref mut list2)) => {
-                let mut carry = 0;
-                let mut sum = list1.val+list2.val;
+            (Some(list1), Some(list2)) => {
+                let sum = list1.val+list2.val;
                 // println!("{:#?}",list1);
-                let mut remainder = sum % 10;
-                carry = (sum-remainder)/10;
-                let mut l3 = ListNode::new(remainder);
-                loop {
-                    // println!("{:#?}",l3);
-                    // println!("{:#?}",list1);
-                    // println!("{:#?}",list2);
-                    if let Some(val1) = list1.pop(){
-                        if let Some(val2) = list2.pop(){
-                            sum = val1+val2+carry;
-                            remainder = sum % 10;
-                            carry = (sum-remainder)/10;
-                            l3.add(remainder);
-                        }else{
-                        sum = val1+carry;
-                        remainder = sum % 10;
-                        carry = (sum-remainder)/10;
-                        l3.add(remainder);
-                        }
-                    }else if let Some(val2) = list2.pop(){
-                        sum = val2+carry;
-                        remainder = sum % 10;
-                        carry = (sum-remainder)/10;
-                        l3.add(remainder);
-                    }else{
-                        if carry != 0{
-                            l3.add(carry)
-                        }
-                        break
-                    }
-                }        
-                return Some(Box::new(l3))
+                // println!("{:#?}",list2);
+                if sum >= 10 {
+                    let remainder = sum % 10;
+                    let carry = (sum-remainder)/10;
+                    let list3 = Some(Box::new(ListNode::new(carry)));
+                    Some(Box::new(ListNode{
+                        val: remainder,
+                        next: Solution::add_two_numbers(Solution::add_two_numbers(list3,list1.next),list2.next),
+                        }))
+                }else {
+                    Some(Box::new(ListNode{
+                        val: sum,
+                        next: Solution::add_two_numbers(list1.next,list2.next),
+                        }))
+                }
             }
         }
     }
